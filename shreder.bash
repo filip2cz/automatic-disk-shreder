e01eac2f5a3690f0dc9417ba5394770dd8755b3f
filip2cz <filip@fkomarek.eu>
2025-11-04 20:17:14 +0100

    ADD: script

---
+++ shreder.bash
@@ -0,0 +1,54 @@
+# Set drive path, for example /dev/sda
+# BE CAREFUL: DRIVE ON THIS PATH WILL BE DELETED, USE COMMAND fdisk -l TO CHECK, WHICH ONE YOU WANT TO ERACE
+config_drive="/dev/sda"
+
+# Set this to your ntfy.sh url
+config_ntfy="ntfy.sh/fkomarek-shreder"
+
+# If this is set to true, script will keep erasing drive until you disconnect it
+config_repeat=true
+
+# Some debug notifications for developers
+config_debug=false
+
+curl -d "Eraser script booted" $config_ntfy
+
+sleep 1
+
+if [ "$config_drive" = "none" ]; then
+    curl -d "config error: change config variables before running script" $config_ntfy
+fi
+
+while ! [ "$config_drive" = "none" ]
+do
+    if test -b $config_drive; then
+        curl -d "Drive to erase found, waiting 10 seconds before shreading" $config_ntfy
+
+        sleep 10
+
+        if ! test -b $config_drive; then
+            curl -d "Drive disconnected before erasing, canceling operation" $config_ntfy
+        else
+            curl -d "Drive is still connected, erasing..." $config_ntfy
+            
+            if shred -v $config_drive; then
+                curl -d "Erasing successfull" $config_ntfy
+
+                if ! config_repeat; then
+                    curl -d "Repeating is disabled, waiting until drive is diconnected" $config_ntfy
+
+                    while test -b $config_drive
+                    do
+                        sleep 1
+                    done
+                fi
+            else
+                curl -d "Erasing failed, trying again" $config_ntfy
+            fi
+        fi
+
+    elif $config_debug; then
+        curl -d "Drive to erase NOT found, trying again in 5 seconds" $config_ntfy
+    fi
+    sleep 5
+done
